9cddbf0fe02f5e7d9a6dec9cc6661954ad82b9ea
Mesut Yılmaz <mesut.yilmaz@stu.fsm.edu.tr>
2021-08-27 12:55:41 +0300

    Edited message

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@
  
      if ( $_SERVER['REQUEST_METHOD']=='GET' && realpath(__FILE__) == realpath( $_SERVER['SCRIPT_FILENAME'] ) ) {        
         header( 'HTTP/1.0 403 Forbidden', TRUE, 403 );
-        die( header( 'location: /index.php' ) );
+        die("Bu sayfaya erişim yetkiniz yok");
     }
 
 function getLatestPosts($count = 5)
